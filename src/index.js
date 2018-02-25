d20d9709ced500218d1cdeb02f9eb540d174a019
DashaKavalenka <mymailDasha@gmail.com>
2018-02-25 15:57:55 +0300

    Implemented a function to solve quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var splitedQuadraticEquation,
+        a,
+        b,
+        c,
+        D,
+        x1,
+        x2,
+        roots;
+    console.log('Initial string is: ' + equation);
+    splitedQuadraticEquation = equation.split(' ');
+    console.log('Array is: ' + splitedQuadraticEquation);
+    a = parseInt(splitedQuadraticEquation[0]);
+    console.log('Parameter A is: ' + a);
+    b = parseInt(splitedQuadraticEquation[3] + splitedQuadraticEquation[4]);
+    console.log('Parameter B is: ' + b);
+    c = parseInt(splitedQuadraticEquation[7] + splitedQuadraticEquation[8]);
+    console.log('Parameter C is: ' + c);
+    D = b*b - 4*a*c;
+    console.log('Parameter D is: ' + D);
+
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+
+    roots = [Math.round(x1), Math.round(x2)];
+    return roots.sort(function (a, b) {return a - b});
 }
